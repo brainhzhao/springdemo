db05111152bcec39b31dafd4ec21c2b93ce79f5d
silencezhao <18722135306@163.com>
2018-07-27 10:17:26 +0800

    commit

--- springdemo/src/priv/zh/main/MianApp.java
+++ springdemo/src/priv/zh/main/MianApp.java
@@ -7,6 +7,7 @@ import priv.zh.bean.Person;
 
 public class MianApp {
 	public static void main(String[] args) {
+		//commit to local repository
 		BeanFactory bf=new ClassPathXmlApplicationContext("Beans.xml");
 		Person person=(Person)bf.getBean("person");
 		System.out.println(person);
